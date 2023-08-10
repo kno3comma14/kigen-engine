71b47dcc3a179bb8334cfd696025231e3686f843
kno3comma14 <enyert.vinas@gmail.com>
2023-08-10 18:11:02 -0500

    First approach to ECS completed

--- src/kigengames/kigen_engine/data/kosmo.clj
+++ src/kigengames/kigen_engine/data/kosmo.clj
@@ -1,15 +1,7 @@
 (ns kigengames.kigen-engine.data.kosmo
-  (:require [nano-id.core :refer [nano-id]]))
-
-(defprotocol ComponentP
-  (create [this]))
-
-(defrecord Component [id instance]
-  ComponentP
-  (create 
-    [_]
-    (let [new-id (nano-id)]
-      (->Component new-id instance))))
+  (:require [kigengames.kigen-engine.data.entity :as entity]
+            [kigengames.kigen-engine.data.component :as component]
+            [nano-id.core :refer [nano-id]]))
 
 (defn- create-pairs [colls]
   (vec
@@ -19,54 +11,18 @@
                     x (first colls)]
                 (cons x more))))))
 
-(defn- study-components
-  [target]
-  (let [is-list? (instance? clojure.lang.PersistentVector target)
-        is-valid? (if (not is-list?)
-                    (instance? Component target)
-                    (every? (fn [c] (instance? Component c)) target))]
-    (if is-valid?
-      is-list?
-      (throw (Exception. "Entities can save component or vectors of components")))))
-
 (defn- filter-components-by-types 
-  [input-types content]
+  [input-types content] 
   (let [cmp-pairs (create-pairs [input-types content])
-        values (map second (filter (fn [x] (= (type (second x)) (first x))) cmp-pairs))]
+        values (map second (filter (fn [x] (= (type (:instance (second x))) (first x))) cmp-pairs))]
     (vec values)))
 
 (defn filter-entities-by-types
-  [input-types entities]
-  (reduce (fn [acc, item] (when (seq(filter-components-by-types input-types item))
+  [input-types entities] 
+  (reduce (fn [acc, item] (when (seq (filter-components-by-types input-types (:backpack item)))
                             (conj acc item)))
           []
           entities))
-
-(defprotocol EntityP
-  (init [this])
-  (disable [this])
-  (add-components [this components]))
-
-(defrecord Entity [id name enabled? backpack]
- EntityP
- 
-  (init 
-  [_]
-  (let [id (nano-id)
-        is-enabled? true] 
-    (->Entity id name is-enabled? (vector))))
-  
-  (disable 
-    [this]
-    (assoc this :enabled false))
-  (add-components [this components]
-    (let [is-list? (study-components components)
-          updated-backpack (if is-list?
-                             (reduce (fn [acc, item] (conj item acc))
-                                     backpack
-                                     components)
-                             (conj backpack components))]
-      (assoc this :backpack updated-backpack))))
 
 (defprotocol KosmoP 
   (create [this new-entities]) 
@@ -83,14 +39,30 @@
   
   (create-entity [this name]
     (let [old-entities entities
-          new-entity (.init (->Entity nil name true []))]
+          new-entity (.init (entity/->Entity nil name true []))]
       (assoc this :entities (conj old-entities new-entity))))
   
   (create-entity [this name components]
     (let [old-entities entities
-          new-entity (.init (->Entity nil name true []))
+          new-entity (.init (entity/->Entity nil name true []))
           entity-with-components (.add-components new-entity components)]
       (assoc this :entities (conj old-entities entity-with-components))))
   
   (find-entities-with [this type-list]
     (filter-entities-by-types type-list entities)))
+
+
+(comment
+  (def sub-component-a "a")
+  (def sub-component-b "b")
+  (def component-a (atom (component/->Component nil sub-component-a)))
+  (def component-b (atom (component/->Component nil sub-component-b)))
+  (reset! component-a (.create @component-a))
+  (reset! component-b (.create @component-b))
+  
+  (def entity-a (atom (entity/->Entity nil "entity-a" true nil)))
+  (reset! entity-a (.init @entity-a))
+  (def entity-b (atom (entity/->Entity nil "entity-b" true nil)))
+  (reset! entity-b (.init @entity-b))
+
+  )
