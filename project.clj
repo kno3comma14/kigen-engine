72d1e2ddb74d88b5d8a42e348f765a35f199f11f
kno3comma14 <enyert.vinas@gmail.com>
2024-07-18 09:44:44 -0500

    Sprites are moving

--- project.clj
+++ project.clj
@@ -9,48 +9,48 @@
   :main ^:skip-aot kigengames.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :shared {:dependencies [[org.lwjgl/lwjgl "3.3.2"]
-                                     [org.lwjgl/lwjgl-opengl "3.3.2"]
-                                     [org.lwjgl/lwjgl-glfw "3.3.2"]
-                                     [org.lwjgl/lwjgl-stb "3.3.2"]
+             :shared {:dependencies [[org.lwjgl/lwjgl "3.3.3"]
+                                     [org.lwjgl/lwjgl-opengl "3.3.3"]
+                                     [org.lwjgl/lwjgl-glfw "3.3.3"]
+                                     [org.lwjgl/lwjgl-stb "3.3.3"]
                                      [org.joml/joml "1.10.5"]
                                      [nano-id "1.0.0"]
                                      [metosin/malli "0.11.0"]]
                       :plugins [[nrepl/lein-nrepl "0.3.2"]]}
              ;; macos-arm64 support
-             :natives-macos-arm64 {:dependencies [[org.lwjgl/lwjgl "3.3.2" :classifier "natives-macos-arm64"]
-                                                  [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-macos-arm64"]
-                                                  [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-macos-arm64"]
-                                                  [org.lwjgl/lwjgl-stb "3.3.2" :classifier "natives-macos-arm64"]]
+             :natives-macos-arm64 {:dependencies [[org.lwjgl/lwjgl "3.3.3" :classifier "natives-macos-arm64"]
+                                                  [org.lwjgl/lwjgl-opengl "3.3.3" :classifier "natives-macos-arm64"]
+                                                  [org.lwjgl/lwjgl-glfw "3.3.3" :classifier "natives-macos-arm64"]
+                                                  [org.lwjgl/lwjgl-stb "3.3.3" :classifier "natives-macos-arm64"]]
                                    :jvm-opts ["-XstartOnFirstThread" "-Djava.awt.headless=true"]}
              :macos-arm64 [:shared :natives-macos-arm64]
              ;; windows-x86 support
-             :natives-windows-x86 {:dependencies [[org.lwjgl/lwjgl "3.3.2" :classifier "natives-windows-x86"]
-                                                  [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-windows-x86"]
-                                                  [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-windows-x86"]
-                                                  [org.lwjgl/lwjgl-stb "3.3.2" :classifier "natives-windows-x86"]]}
+             :natives-windows-x86 {:dependencies [[org.lwjgl/lwjgl "3.3.3" :classifier "natives-windows-x86"]
+                                                  [org.lwjgl/lwjgl-opengl "3.3.3" :classifier "natives-windows-x86"]
+                                                  [org.lwjgl/lwjgl-glfw "3.3.3" :classifier "natives-windows-x86"]
+                                                  [org.lwjgl/lwjgl-stb "3.3.3" :classifier "natives-windows-x86"]]}
              :windows-x86 [:shared :natives-windows-x86]
              ;; windows-amd64 support 
-             :natives-windows-amd64 {:dependencies [[org.lwjgl/lwjgl "3.3.2" :classifier "natives-windows"]
-                                                    [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-windows"]
-                                                    [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-windows"]
-                                                    [org.lwjgl/lwjgl-stb "3.3.2" :classifier "natives-windows"]]}
+             :natives-windows-amd64 {:dependencies [[org.lwjgl/lwjgl "3.3.3" :classifier "natives-windows"]
+                                                    [org.lwjgl/lwjgl-opengl "3.3.3" :classifier "natives-windows"]
+                                                    [org.lwjgl/lwjgl-glfw "3.3.3" :classifier "natives-windows"]
+                                                    [org.lwjgl/lwjgl-stb "3.3.3" :classifier "natives-windows"]]}
              :windows-amd64 [:shared :natives-windows-amd64]
              ;; linux-amd64 support
-             :natives-linux-amd64 {:dependencies [[org.lwjgl/lwjgl "3.3.2" :classifier "natives-linux"]
-                                                  [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-linux"]
+             :natives-linux-amd64 {:dependencies [[org.lwjgl/lwjgl "3.3.3" :classifier "natives-linux"]
+                                                  [org.lwjgl/lwjgl-opengl "3.3.3" :classifier "natives-linux"]
                                                   [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-linux"]
                                                   [org.lwjgl/lwjgl-stb "3.3.2" :classifier "natives-linux"]]}
              :linux-amd64 [:shared :natives-linux-amd64]
              ;; linux-arm64 support
-             :natives-linux-arm64 {:dependencies [[org.lwjgl/lwjgl "3.3.2" :classifier "natives-linux-arm64"]
-                                                  [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-linux-arm64"]
-                                                  [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-linux-arm64"]
-                                                  [org.lwjgl/lwjgl-stb "3.3.2" :classifier "natives-linux-arm64"]]}
+             :natives-linux-arm64 {:dependencies [[org.lwjgl/lwjgl "3.3.3" :classifier "natives-linux-arm64"]
+                                                  [org.lwjgl/lwjgl-opengl "3.3.3" :classifier "natives-linux-arm64"]
+                                                  [org.lwjgl/lwjgl-glfw "3.3.3" :classifier "natives-linux-arm64"]
+                                                  [org.lwjgl/lwjgl-stb "3.3.3" :classifier "natives-linux-arm64"]]}
              :linux-arm64 [:shared :natives-linux-arm64]
              ;; linux-arm32 support
-             :natives-linux-arm32 {:dependencies [[org.lwjgl/lwjgl "3.3.2" :classifier "natives-linux-arm32"]
-                                                  [org.lwjgl/lwjgl-opengl "3.3.2" :classifier "natives-linux-arm32"]
-                                                  [org.lwjgl/lwjgl-glfw "3.3.2" :classifier "natives-linux-arm32"]
-                                                  [org.lwjgl/lwjgl-stb "3.3.2" :classifier "natives-linux-arm32"]]}
+             :natives-linux-arm32 {:dependencies [[org.lwjgl/lwjgl "3.3.3" :classifier "natives-linux-arm32"]
+                                                  [org.lwjgl/lwjgl-opengl "3.3.3" :classifier "natives-linux-arm32"]
+                                                  [org.lwjgl/lwjgl-glfw "3.3.3" :classifier "natives-linux-arm32"]
+                                                  [org.lwjgl/lwjgl-stb "3.3.3" :classifier "natives-linux-arm32"]]}
              :linux-arm32 [:shared :natives-linux-arm32]})
--- src/kigengames/kigen_engine/rendering/renderer.clj
+++ src/kigengames/kigen_engine/rendering/renderer.clj
@@ -5,27 +5,36 @@
 
 (defprotocol RendererP
   (add-drawable [this target])
-  (render [this]))
+  (render [this dt]))
 
 (defrecord Renderer [batches]
   RendererP
-  (add-drawable 
-    [_ target] 
+  (add-drawable
+    [_ comp_target]
     (let [added (atom false)
-          batch (first (filter (fn [item] @(:has-capacity item)) @batches))]
-      (when (not= batch nil) 
+          batch (first (filter (fn [item] @(:has-capacity item)) @batches))
+          target (:instance comp_target)]
+      (when (not= batch nil)
         (.add-sprite batch target)
-        (reset! added true)) 
+        (reset! added true))
       (when (not @added)
-        (let [new-br (br/create max-batch-size)] 
+        (let [new-br (br/create max-batch-size)]
           (.start new-br)
           (.add-sprite new-br target)
           (swap! batches conj new-br)))))
 
-  (render [_]
-    (reduce (fn [_, item] 
-              (.render item)) 
-            nil 
+  (render [_ dt]
+    (reduce (fn [_, item]
+              (let [sprite-list (filter (fn [s] (not (nil? s))) @(:sprites item))]
+                (reduce (fn [acc, _]
+                          (let [sprite-renderer (nth sprite-list acc)
+                                new-sprite-renderer (.process sprite-renderer sprite-renderer dt)]
+                            (swap! (:sprites item) assoc acc new-sprite-renderer)
+                            (.render item))
+                          (inc acc))
+                        0
+                        sprite-list)))
+            nil
             @batches)))
 
 (defn create []
